7748921378fb57db96cb029ef3f0d124694ff5e5
gr1tEnt <129974448+gr1tEnt@users.noreply.github.com>
2025-05-20 16:40:55 +0300

    Update solution - CodeHub

--- 8-kyu/even-or-odd/even-or-odd.java
+++ 8-kyu/even-or-odd/even-or-odd.java
@@ -1,9 +1,5 @@
 public class Kata {
     public static String evenOrOdd(int number) {
-      if (number % 2 == 0) {
-        return "Even";
-      } else {
-        return "Odd";
-      }
+        return (number % 2 == 0) ? "Even" : "Odd";
     }
 }
