e2c6915c2d1d41fc1cf5d1ffbcaa06c47fe16733
gr1tEnt <129974448+gr1tEnt@users.noreply.github.com>
2025-05-26 15:25:01 +0300

    Add solution - CodeHub

---
+++ undefined/undefined/undefined.java
@@ -0,0 +1,9 @@
+class Kata {
+    public static String getMiddle(String word) {
+        if (word.length() % 2 != 0) {
+            return String.valueOf(word.charAt(word.length() / 2));
+        } else {
+            return word.substring(word.length() / 2 - 1, word.length() / 2 + 1);
+        }
+    }
+}
