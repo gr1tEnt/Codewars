72aacc0fdc70a5cf7799649c7d88166c16d01b3d
gr1tEnt <129974448+gr1tEnt@users.noreply.github.com>
2025-12-27 20:58:28 +0200

    Add solution - CodeHub

---
+++ 7-kyu/make-a-function-that-does-arithmetic/make-a-function-that-does-arithmetic.java
@@ -0,0 +1,11 @@
+class ArithmeticFunction {
+    public static int arithmetic(int a, int b, String operator) {
+        return switch (operator) {
+            case "add" -> a + b;
+            case "subtract" -> a - b;
+            case "multiply" -> a * b;
+            case "divide" -> a / b;
+            default -> 0;
+        };
+    }
+}
