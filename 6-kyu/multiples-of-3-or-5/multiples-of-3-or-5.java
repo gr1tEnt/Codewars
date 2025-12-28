cba728fbd99e6df8c76a11f15de597040670dafc
gr1tEnt <129974448+gr1tEnt@users.noreply.github.com>
2025-12-28 14:41:53 +0200

    Add solution - CodeHub

---
+++ 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.java
@@ -0,0 +1,12 @@
+public class Solution {
+​
+    public int solution(int number) {
+        int res = 0;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                res += i;
+            }
+        }
+        return res;
+    }
+}
