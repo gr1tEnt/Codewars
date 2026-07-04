f01b14790f93fdba60cc43f021b20d8b79b060ed
gr1tEnt <129974448+gr1tEnt@users.noreply.github.com>
2026-07-04 16:17:50 +0200

    Add solution - CodeHub

---
+++ 8-kyu/return-the-day/return-the-day.java
@@ -0,0 +1,15 @@
+public class DayOfWeek {
+    public static String getDay(int n) {
+​
+        return switch (n) {
+            case 1 -> "Sunday";
+            case 2 -> "Monday";
+            case 3 -> "Tuesday";
+            case 4 -> "Wednesday";
+            case 5 -> "Thursday";
+            case 6 -> "Friday";
+            case 7 -> "Saturday";
+            default -> "Wrong, please enter a number between 1 and 7";
+        };
+    }
+}
